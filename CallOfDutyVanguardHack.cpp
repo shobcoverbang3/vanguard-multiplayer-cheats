d31051bdee34c269ea82e865cfaf9aa3c11055a9
shobcoverbang3 <angelampyw3@gmail.com>
2025-06-20 15:39:39 -0400

    Add files via upload

---
+++ CallOfDutyVanguardHack.cpp
@@ -0,0 +1,7 @@
+// WTKK8O6q2E
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello from function e9781047066c4086833c2020545fecf8_20250619080304891452!" << endl;
+    return 0;
+}
---
+++ CallOfDutyVanguardHack.rb
@@ -0,0 +1,5 @@
+# WTKK8O6q2E
+def method_fjg1T
+    puts "Hello from function e9781047066c4086833c2020545fecf8_20250619080304891452!"
+end
+method_fjg1T
